dac7a24a63836ab3a2e235c275c0143c04d33bc0
Johnlin <johno1001741060168@gmail.com>
2026-05-19 10:37:26 +0800

    2026-05-19

---
+++ 2026-05-19/ch11-1-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>ch11-1-1.php</title>
+</head>
+<body>
+<?php
+$link = @mysqli_connect(
+   'johnlin',
+   'root',
+   'KydGcOdIbdYezedVcyOEnkDdMPZMPexn',
+   'myschool');
+
+if ( !$link ) {
+   echo 'MySQL資料庫連接錯誤!<br/>';
+   exit();
+}
+else {
+   echo 'MySQL資料庫myschool連接成功!<br/>';
+}
+mysqli_close($link);
+?>
+</body>
+</html>
---
+++ 2026-05-19/ch11-2-1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>ch11-2-1.php</title>
+</head>
+<body>
+<?php
+$link = mysqli_connect(
+   'johnlin',
+   'root',
+   'KydGcOdIbdYezedVcyOEnkDdMPZMPexn',
+   'myschool')
+   or die('無法開啟 MySQL 資料庫連接!<br/>');
+
+echo '資料庫myschool開啟成功!<br/>';
+$sql = 'SELECT * FROM students';
+echo 'SQL查詢字串: $sql <br/>';
+
+// 送出查詢的SQL指令
+if ( $result = mysqli_query($link, $sql) ) { 
+   echo '<b>學生資料:</b><br/>';
+   while( $row = mysqli_fetch_assoc($result) ){ 
+      echo $row["sno"].'-'.$row["name"].'-'.$row["birthday"].'<br/>';
+   }
+   mysqli_free_result($result);
+}
+mysqli_close($link);
+?>
+</body>
+</html>
